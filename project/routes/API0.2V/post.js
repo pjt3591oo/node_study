8be0d175b01dce7031778229870f0f48397dd129
pjt3591oo <pjt3591oo@gmail.com>
2017-08-05 17:47:18 +0900

    6주차 내용 추가한다

--- project/routes/API0.2V/post.js
+++ project/routes/API0.2V/post.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'root',
+	password: '11111111',
+	database: 'board'
+});
+
 // get     전체 글 보기, 특정 글 보기
 // post    글 쓰기
 // put     글 수정
@@ -10,35 +19,64 @@ var router = express.Router();
 
 router.get('/', function(req, res){
     let page = req.query.page || 1;
+    client.query('SELECT *FROM board', [], function(err, data){
+        res.render('post', {postList: data});
+    });
+})
 
-    // SELECT *FROM board ;
-    res.send('get test');
+//게시글 작성 페이지
+router.get('/edit', function(req, res){
+    res.render('edit');
+})
+
+//게시글 수정 페이지
+router.get('/edit/:id', function(req, res){
+	let boardId = req.params.id;
+
+	client.query('SELECT *FROM board WHERE id = ?', [boardId], function(err, data){
+		console.log(data[0])
+		res.render('modi', {
+			id: data[0]['id'],
+			title: data[0]['title'],
+			content: data[0]['content'],
+			author: data[0]['author']
+		});
+
+
+	})
 })
 
 router.post('/', function(req, res){
     let title = req.body.title;
     let content = req.body.content;
+    let author = req.body.author;
 
-    // 'INSERT INTO board(title, content) VALUES(?, ?)', [title, content]
+    let sql = 'INSERT INTO board(title, content, author) VALUES(?, ?, ?)';
 
-    res.send('post test');
+    client.query(sql, [title, content, author], function(){
+        res.redirect('/APIV0.2/post');
+    });
+
 })
 
-router.put('/', function(req, res){
+router.post('/modi', function(req, res){
     let title = req.body.title;
     let content = req.body.content;
-    let postId = req.body.postId;
+    let author = req.body.author;
+    let boardId = req.body.boardId;
 
-    //'UPDATE board SET title=?, content=? WHERE id =?'  , [title, content, postId]
+    let sql = 'UPDATE board SET title=?, content=?, author=? WHERE id =?'
 
-    res.send('put test');
+	client.query(sql, [title, content, author, boardId], function(err){
+
+		res.redirect('/APIV0.2/post');
+	})
+
 })
 
 router.delete('/', function(req, res){
     let postId = req.body.postId;
-
     // 'DELETE FROM board WHERE id=?', [postId]
-
     res.send('delete test');
 })
 
